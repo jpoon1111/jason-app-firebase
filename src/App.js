2bcee4f3566f3d28707ecd168de69b380849c3d9
Jason <jaypoon1111@gmail.com>
2025-12-02 01:19:41 -0800

    First check in

--- src/App.js
+++ src/App.js
@@ -1,22 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { registerVersion } from "firebase/app";
+import "./App.css";
+import logo from "./assets/frontend-simplified-logo.png";
+import React from "react";
+import { auth } from "./firebase/init";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+library.add(faBars);
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
+  //Sign up
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  //Sign in
+  function login() {
+    console.log("login");
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  //Sign out
+  // function logout() {
+  //   console.log("logout");
+  //   signOut(auth);
+  //   setUser({});
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="row">
+          <nav>
+            <div className="fes__header">
+              <div>
+                <FontAwesomeIcon icon="bars" />
+              </div>
+
+              <figure>
+                <img src={logo} className="header__img"></img>
+              </figure>
+            </div>
+            <div className="fes__header fes__nav">
+              {
+                user?              <>
+                <button className="btn" onClick={login}>Login</button>
+                <button className="btn account__register" onClick={register}  >Register</button>
+              </>
+              :                 <div className="login__user--logo">
+                  <h2>{loading ? "Loading..." : user.email[0]}</h2>
+                </div>
+              }
+
+              {/* <button onClick={logout}>Logout</button> */}
+
+            </div>
+          </nav>
+        </div>
       </header>
     </div>
   );
---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+
+  // TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA71h-tnm5IZkXF5llr3XZ-cBBpXy-O0iU",
+  authDomain: "jason-app-firebase.firebaseapp.com",
+  projectId: "jason-app-firebase",
+  storageBucket: "jason-app-firebase.firebasestorage.app",
+  messagingSenderId: "274363731588",
+  appId: "1:274363731588:web:882ba29934c6adadd1bcd1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
